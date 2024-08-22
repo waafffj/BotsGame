0a54077f0abd866acaace4b5d03fa105d0b6dbeb
wangkeming <376086637@qq.com>
2024-08-23 01:28:29 +0800

    我的BOts

---
+++ backend/src/main/java/com/kob/backend/service/impl/user/account/RegisterServiceImpl.java
@@ -0,0 +1,67 @@
+package com.kob.backend.service.impl.user.account;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import com.kob.backend.service.user.account.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class RegisterServiceImpl implements RegisterService {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public Map<String, String> register(String username, String password, String confirmedPassword) {
+        Map<String,String> map = new HashMap<>();
+        if(username == null ){
+            map.put("error_message","用户名不能为空");
+            return map;
+        }
+        if(password == null || confirmedPassword == null ){
+            map.put("error_message","密码不能为空");
+            return map;
+        }
+        username = username.trim();
+        if(username.length() == 0){
+            map.put("error_message","用户名不能为空");
+            return map;
+        }
+        if(username.length() > 100){
+            map.put("error_message","用户名长度不能大于100");
+            return map;
+        }
+        if(password.length() > 100 || confirmedPassword.length() > 100){
+            map.put("error_message","密码长度不能大于100");
+            return map;
+        }
+        if(!password.equals(confirmedPassword)){
+            map.put("error_message","两次输入的密码不一致");
+            return map;
+        }
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username",username);
+        List<User> users = userMapper.selectList(queryWrapper);
+        if(!users.isEmpty()){
+            map.put("error_message","用户名已存在");
+            return map;
+        }
+
+
+        String  encodedPassword = passwordEncoder.encode(password);
+        String photo = "https://ts1.cn.mm.bing.net/th/id/R-C.a3326dfea8ea5544ebd406b824137ee9?rik=c6C3lRiXIQoEYg&riu=http%3a%2f%2fimg.touxiangwu.com%2fzb_users%2fupload%2f2022%2f11%2f202211021667379370320460.jpg&ehk=VTjaG57lKTodjm5E3FWOUlJf%2fWxUbyZ8lw%2bMXV2END4%3d&risl=&pid=ImgRaw&r=0";
+        User user = new User(null,photo,username,encodedPassword);
+        userMapper.insert(user);
+
+        map.put("error_message","success");
+        return  map;
+    }
+}
